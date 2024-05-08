6e34634dcad10800df3466aeec4f571746e89824
raquelgl20 <161474670+raquelgl20@users.noreply.github.com>
2024-05-08 09:16:41 -0300

    Add files via upload

---
+++ let valor1 = prompt ('informe o primeiro.js
@@ -0,0 +1,12 @@
+let valor1 = prompt ('informe o primeiro valor: ');
+let valor2 = prompt ('informe o segundo valor: ');
+
+
+
+if((valor1>=0&&valor2>=0)||(valor1<0&&valor2<0)){
+
+soma == valor1+valor2;
+console.log('a soma dos dois valores é: '+soma);
+} else {
+    alert('valor inválido!!! ');
+}
